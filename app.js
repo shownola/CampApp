2274310f9ab017e9709ae86bd3d97504752fe41f
shownola <swasieko@gmail.com>
2020-07-10 16:19:31 -0700

    Create initial routes and views

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 var express = require('express');
 var app = express();
 
-app.listen(process.env.PORT, process.env.IP, function(){
-  console.log('The CampApp server has started!')
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+  res.render('landing');
 });
+
+app.get('/campgrounds', function(req, res){
+  var campgrounds = [
+    {name: 'Salmon Creek', image: "https://images.pexels.com/photos/699558/pexels-photo-699558.jpeg?auto=compress&cs=tinysrgb&h=350"},
+    {name: 'Granite Hill', image: "https://images.pexels.com/photos/1061640/pexels-photo-1061640.jpeg?auto=compress&cs=tinysrgb&h=350"},
+    {name: 'Mountain Goat Rest', image: "https://images.pexels.com/photos/2398220/pexels-photo-2398220.jpeg?auto=compress&cs=tinysrgb&h=350"},
+  ]
+  res.render('campgrounds', {campgrounds:campgrounds});
+});
+
+app.listen(3000, '127.0.0.1', function(){
+  console.log('The Campapp server has started!')
+})
+
+// app.listen(process.env.PORT, process.env.IP, function(){
+//   console.log('The CampApp server has started!')
+// });
