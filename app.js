9e65b1e8a1912ed043c2be8d7a254cb7ff6127a5
shownola <swasieko@gmail.com>
2020-07-14 16:04:46 -0700

    Implement show functionality

--- app.js
+++ app.js
@@ -4,28 +4,30 @@ var express     = require('express'),
 
 const mongoose  = require('mongoose');
 
+mongoose.connect('mongodb://localhost:27017/camp_app', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('Connected to DB!'))
+.catch(error => console.log(error.message));
 
-mongoose.connect('mongodb://localhost/camp_app');
+// mongoose.connect('mongodb://localhost/camp_app');
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
 
-// mongoose.connect('mongodb://localhost:27017/db_name', {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true
-// })
-// .then(() => console.log('Connected to DB!'))
-// .catch(error => console.log(error.message));
+
 
 // SCHEMA SETUP
 var campgroundSchema = new mongoose.Schema({
   name: String,
-  image: String
+  image: String,
+  description: String,
 });
 
 var Campground = mongoose.model('Campground', campgroundSchema);
 
-// Campground.create({name: 'Salmon Creek', image: "https://images.pexels.com/photos/699558/pexels-photo-699558.jpeg?auto=compress&cs=tinysrgb&h=350"}, function(err, campground){
+// Campground.create({name: 'Salmon Creek', image: "https://images.pexels.com/photos/699558/pexels-photo-699558.jpeg?auto=compress&cs=tinysrgb&h=350", description: 'A scenic 150-acre Civil War era farm is graced with a perfect mix of woodlands, tree-dotted meadows and grassy rolling hills. Regardless of your personal definition of camping, you will always find precisely what you need at Granite Hill. GHCR offers private tent sites, spacious pull-thru sites catering to the largest RVs and Big Rigs, as well as a wide range of options in group camping and rustic cabins in a wooded setting'},  function(err, campground){
 //   if(err){
 //     console.log(err);
 //   } else {
@@ -40,26 +42,30 @@ app.get('/', function(req, res){
   res.render('landing');
 });
 
+// INDEX - shows all campgrounds
 app.get('/campgrounds', function(req, res){
   Campground.find({}, function(err, allCampgrounds){
     if(err){
       console.log(err);
     } else {
-      res.render('campgrounds', {campgrounds:allCampgrounds});
+      res.render('index', {campgrounds:allCampgrounds});
     }
   });
   // res.render('campgrounds', {campgrounds:campgrounds});
 });
 
+// NEW - show form to create new campground
 app.get('/campgrounds/new', function(req, res){
   res.render('new.ejs')
 })
 
+// CREATE CAMPGROUND:
 app.post('/campgrounds', function(req, res){
   // get data from form and add to cgs array then redirect back to the cgs page
   var name = req.body.name;
   var image = req.body.image;
-  var newCampground = {name: name, image: image};
+  var desc = req.body.description;
+  var newCampground = {name: name, image: image, description: desc};
   // campgrounds.push(newCampground);
   // Create a new campground and save to DB
   Campground.create(newCampground, function(err, newlyCreated){
@@ -69,6 +75,17 @@ app.post('/campgrounds', function(req, res){
       res.redirect('/campgrounds');
     }
   });
+});
+
+// SHOW
+app.get('/campgrounds/:id', function(req, res){
+  Campground.findById(req.params.id, function(err, foundCampground){
+    if(err){
+      console.log(err);
+    } else {
+      res.render('show', {campground: foundCampground})
+    }
+  })
 });
 
 
