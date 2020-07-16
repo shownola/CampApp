d51f91301592552aa642d4af5ee82a998f7a5e2e
shownola <swasieko@gmail.com>
2020-07-16 13:19:51 -0700

    Configure passport, create user registration file and form and setup registration routes

--- app.js
+++ app.js
@@ -21,6 +21,18 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 seedDB();
 
+// PASSPORT CONFIGURATION
+app.use(require('express-session')({
+  secret: 'Once again Rusty wins',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 app.get('/', function(req, res){
   res.render('landing');
@@ -105,6 +117,22 @@ app.post('/campgrounds/:id/comments', function(req, res){
   })
 });
 
+// AUTH ROUTES
+app.get('/register', function(req, res){
+  res.render('register');
+});
+app.post('/register', function(req, res){
+  var newUser = new User({username: req.body.username});
+  User.register(newUser, req.body.password, function(err, user){
+    if(err){
+      console.log(err);
+      return res.render('register')
+    }
+    passport.authenticate('local')(req, res, function(){
+      res.redirect('/campgrounds');
+    })
+  })
+});
 
 
 
