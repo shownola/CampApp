d192f1efb796751e18f077e8b14ba820c7f0cc23
shownola <swasieko@gmail.com>
2020-07-16 14:04:10 -0700

    Implement Login functionality with routes

--- app.js
+++ app.js
@@ -130,8 +130,21 @@ app.post('/register', function(req, res){
     }
     passport.authenticate('local')(req, res, function(){
       res.redirect('/campgrounds');
-    })
-  })
+    });
+  });
+});
+
+// Login
+app.get('/login', function(req, res){
+  res.render('login');
+});
+
+app.post('/login', passport.authenticate('local',
+  {
+    successRedirect: '/campgrounds',
+    failureRedirect: '/login'
+  }), function(req, res){
+
 });
 
 
