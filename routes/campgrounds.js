6f2bf85ba8177189e50cadfcce7238080fa6a208
shownola <swasieko@gmail.com>
2020-07-17 11:33:14 -0700

    Implement destroy functionality for campgrounds and fix for btn-xs

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -73,7 +73,18 @@ router.put('/:id', function(req, res){
       res.redirect('/campgrounds/' + req.params.id);
     }
   })
-})
+});
+
+// Destroy
+router.delete('/:id', function(req, res){
+  Campground.findByIdAndRemove(req.params.id, function(err){
+    if(err){
+      res.redirect('/campgrounds');
+    } else {
+      res.redirect('/campgrounds');
+    }
+  })
+});
 
 // middleware
 function isLoggedIn(req, res, next){
