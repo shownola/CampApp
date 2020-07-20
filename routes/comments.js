66830e8ea0349c18fd36e681190cdcce294e9b0a
shownola <swasieko@gmail.com>
2020-07-20 16:23:18 -0700

    Implement Update functionality for editing comments

--- routes/comments.js
+++ routes/comments.js
@@ -40,6 +40,28 @@ router.post('/', isLoggedIn, function(req, res){
   })
 });
 
+// Edit Comments
+router.get('/:comment_id/edit', function(req, res){
+  Comment.findById(req.params.comment_id, function(err, foundComment){
+    if(err){
+      res.redirect('back');
+    } else {
+      res.render('comments/edit', {campground_id: req.params.id, comment: foundComment});
+    }
+  });
+});
+
+// Update Comments
+router.put('/:comment_id', function(req, res){
+  Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, UpdatedComment){
+    if(err){
+      res.redirect('back');
+    } else {
+      res.redirect('/campgrounds/' + req.params.id);
+    }
+  });
+});
+
 // middleware
 function isLoggedIn(req, res, next){
   if(req.isAuthenticated()){
